a0e7f90f6b23d42e938717da3e0b9c4f1d44306a
Koushik Naskar <koushik.naskar9@gmail.com>
2022-02-20 21:29:03 +0530

    check

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('./README.md') as f:
 
 setup(
     name='koutils',
-    version='0.0.3',
+    version='0.0.4',
     description='A common file operation utility',
     long_description=txt,
     author='Koushik Naskar',
