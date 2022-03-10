b92018fdcf45c0a2f61220d371e5423eff5e438b
Koushik Naskar <koushik.naskar9@gmail.com>
2022-03-10 18:33:17 +0530

    update

--- kfutils/cli.py
+++ kfutils/cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from __future__ import absolute_import, unicode_literals,division, print_function
+from __future__ import absolute_import, unicode_literals, division, print_function
 
 import os
 import sys
@@ -11,46 +11,69 @@ import glob
 import numpy as np
 
 
-
 class CustomParser(argparse.ArgumentParser):
+
     def error(self, message):
         sys.stderr.write('\033[91mError: %s\n\033[0m' % message)
         self.print_help()
         sys.exit(2)
 
 
-
 def listOfInts(val):
     try:
-        val =int(val) 
-        if val<0:
+        val = int(val)
+        if val < 0:
             raise ValueError
     except:
         raise argparse.ArgumentTypeError("Only list of positive are allowed")
     return val
 
 
-
-
 def createParser():
     #main parser
-    parser = CustomParser(
-        prog="kutils",
-        formatter_class=argparse.RawTextHelpFormatter,
-        description = "A tool for common file operations."
-        )
+    parser = CustomParser(prog="kutils",
+                          formatter_class=argparse.RawTextHelpFormatter,
+                          description="A tool for common file operations.")
 
     #adding options for numerical jobs
-    parser.add_argument("--ifile",'-i',type= str,help= "File name",metavar= "FILE",required = True)
-    parser.add_argument('--ofile','-o',type=str, help="Output file name", metavar="FILE")
-    parser.add_argument('--cols','-c', help="index of grid columns", nargs='+', metavar='COLS',required=True, type=listOfInts)
-    parser.add_argument('--rad2deg','-rd', help="index of columns to convert to degree from radian", nargs='+', metavar='COLS', type=listOfInts)
-    parser.add_argument('--deg2rad','-dr', help="index of columns to convert to radian to degree", nargs='+', metavar='COLS', type=listOfInts)
-    parser.add_argument('--dropcols','-dc', help="index of columns to drop", nargs='+', metavar='COLS', type=listOfInts)
+    parser.add_argument("--ifile",
+                        '-i',
+                        type=str,
+                        help="File name",
+                        metavar="FILE",
+                        required=True)
+    parser.add_argument('--ofile',
+                        '-o',
+                        type=str,
+                        help="Output file name",
+                        metavar="FILE")
+    parser.add_argument('--cols',
+                        '-c',
+                        help="index of grid columns",
+                        nargs='+',
+                        metavar='COLS',
+                        required=True,
+                        type=listOfInts)
+    parser.add_argument(
+        '--rad2deg',
+        '-rd',
+        help="index of columns to convert to degree from radian",
+        nargs='+',
+        metavar='COLS',
+        type=listOfInts)
+    parser.add_argument('--deg2rad',
+                        '-dr',
+                        help="index of columns to convert to radian to degree",
+                        nargs='+',
+                        metavar='COLS',
+                        type=listOfInts)
+    parser.add_argument('--dropcols',
+                        '-dc',
+                        help="index of columns to drop",
+                        nargs='+',
+                        metavar='COLS',
+                        type=listOfInts)
     return parser.parse_args()
-
-
-
 
 
 def main():
@@ -59,30 +82,32 @@ def main():
 
     cols = args.cols
 
-    # read file 
+    # read file
     data = np.loadtxt(inpFile)
 
     # rad to deg
-    if(args.rad2deg): 
-        data[:,args.rad2deg] = np.rad2deg(data[:,args.rad2deg])
+    if (args.rad2deg):
+        data[:, args.rad2deg] = np.rad2deg(data[:, args.rad2deg])
     # deg to rad
-    if(args.deg2rad): 
-        data[:,args.deg2rad] = np.deg2rad(data[:,args.deg2rad])
+    if (args.deg2rad):
+        data[:, args.deg2rad] = np.deg2rad(data[:, args.deg2rad])
     # delete columns
-    if(args.dropcols): 
+    if (args.dropcols):
         data = np.delete(data, args.dropcols, axis=1)
 
     # now write file
     outFile = args.ofile
     if not outFile: outFile = inpFile + '_out'
 
-
-    if len(cols)<1: #1D file
+    if len(cols) < 1:  #1D file
         np.savetxt(outFile, data, delimiter='\t', fmt='%.8f')
     else:
-        with open(outFile,'w') as f:
-            for th in np.unique(data[:,cols[0]]):
-                np.savetxt(f,data[data[:,cols[0]]==th], delimiter='\t', fmt='%.8f')
+        with open(outFile, 'w') as f:
+            for th in np.unique(data[:, cols[0]]):
+                np.savetxt(f,
+                           data[data[:, cols[0]] == th],
+                           delimiter='\t',
+                           fmt='%.8f')
                 f.write('\n')
 
     print(f"File saved as {outFile}")
--- setup.py
+++ setup.py
@@ -3,33 +3,29 @@ from setuptools import setup, find_packages
 with open('./README.md') as f:
     txt = f.read()
 
-
-setup(
-    name='kfutils',
-    version='0.0.7',
-    description='A common file operation utility',
-    long_description=txt,
-    author='Koushik Naskar',
-    author_email='koushik.naskar9@gmail.com',
-    license = "MIT",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: System :: Shells'
-    ],
-    keywords='File Operations',
-    project_urls={'Source Code':'https://github.com/Koushikphy/kutils'},
-    zip_safe=True,
-    python_requires='>=2.7',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'kfutils = kfutils.cli:main',
-        ],
-    }
-)
+setup(name='kfutils',
+      version='0.0.7',
+      description='A common file operation utility',
+      long_description=txt,
+      author='Koushik Naskar',
+      author_email='koushik.naskar9@gmail.com',
+      license="MIT",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console', 'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: System :: Shells'
+      ],
+      keywords='File Operations',
+      project_urls={'Source Code': 'https://github.com/Koushikphy/kutils'},
+      zip_safe=True,
+      python_requires='>=2.7',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'kfutils = kfutils.cli:main',
+          ],
+      })
