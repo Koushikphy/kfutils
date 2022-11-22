d7331ffef0f5e4145aed3ee12e31ee84893ef3a7
Koushik Naskar <Koushik.naskar9@gmail.com>
2022-11-22 11:36:29 +0530

    credit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md') as f:
     txt = f.read()
 
 setup(name='kfutils',
-    version='0.1.4',
+    version='0.1.5',
     description='A tool for common data file operation.',
     long_description=txt,
     long_description_content_type='text/markdown',
